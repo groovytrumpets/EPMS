e4a86d0a1f19e303cc9f3a5d3ea4492d71ebafdd
khanhnnhe181337 <nguye@LAPTOP-OF-KHANH>
2025-07-09 00:53:12 +0700

    add all models

---
+++ src/java/model/ActivityLog.java
@@ -0,0 +1,62 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDateTime;
+
+/**
+ *
+ * @author nguye
+ */
+public class ActivityLog {
+    private int logId;
+    private int userId;
+    private String action;
+    private LocalDateTime createDate;
+
+    public ActivityLog(int logId, int userId, String action, LocalDateTime createDate) {
+        this.logId = logId;
+        this.userId = userId;
+        this.action = action;
+        this.createDate = createDate;
+    }
+
+    public ActivityLog() {
+    }
+
+    public int getLogId() {
+        return logId;
+    }
+
+    public void setLogId(int logId) {
+        this.logId = logId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getAction() {
+        return action;
+    }
+
+    public void setAction(String action) {
+        this.action = action;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+    
+
+}
---
+++ src/java/model/User.java
@@ -0,0 +1,141 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+/**
+ *
+ * @author nguye
+ */
+/*
+(<Username, nvarchar(50),>
+           ,<Password, nvarchar(200),>
+           ,<Status, nvarchar(20),>
+           ,<FullName, nvarchar(100),>
+           ,<Phone, nvarchar(15),>
+           ,<CreateDate, datetime,>
+           ,<Dob, date,>
+           ,<Email, nvarchar(100),>
+           ,<Gender, nvarchar(10),>
+           ,<RoleId, int,>)
+GO
+*/
+public class User {
+    private int userId;
+    private String userName;
+    private int roleId;
+    private String status;
+    private LocalDateTime createDate;
+    private Date dob;
+    private String email, phone, password,fullName, gender;
+
+    public User() {
+    }
+
+    public User(int userId, String userName, int roleId, String status, LocalDateTime createDate, Date dob, String email, String phone, String password, String fullName, String gender) {
+        this.userId = userId;
+        this.userName = userName;
+        this.roleId = roleId;
+        this.status = status;
+        this.createDate = createDate;
+        this.dob = dob;
+        this.email = email;
+        this.phone = phone;
+        this.password = password;
+        this.fullName = fullName;
+        this.gender = gender;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public LocalDateTime getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(LocalDateTime createDate) {
+        this.createDate = createDate;
+    }
+
+    public Date getDob() {
+        return dob;
+    }
+
+    public void setDob(Date dob) {
+        this.dob = dob;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+    
+}
